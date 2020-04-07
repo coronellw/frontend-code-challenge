25af44e46b4e7103127bc9111fd68c817171b76f
Wiston Coronell <coronell@gmail.com>
2020-04-07 17:12:15 -0500

    adding answers to question

--- README.md
+++ README.md
@@ -1,45 +1,29 @@
 # Front-end Code Challenge - React
 
-This is a boilerplate for a code-challenge meant for Front-end engineers using React as library tool for development. It includes all necessary HTML and CSS, so your work is to focus on the development of the app. Please fork this repository in your own account.
+## Questions
+### Explain the difference between a var, a let, and a const.
 
-### For other technologies
+The main difference is the scope that the variables receives, being `var` more _permisisve_ , while `let` and `const` has a strict _block scope_ and the difference between `let` and `const` relies in that the `let` variables can be reassigned another value, while the const is a value that will not change through the execution of the code.
 
-Switch to the branch of the technology of your expertise and follow the README:
+### What are the differences between declaring a function with the keyword function against creating it with an arrow function?
 
-```bash
-git checkout [react-native|angular]
+Once again the answer is in the scope that `this` keyword will have, when using the keyword `function` then `this` will be bind to the function context and will not have access to the outer variables. while if we use the `arrow` function approarch then the `this` keyword will have access to its parent variables.
+
+### How would you turn a method with a callback argument into a promise?
+
+```javascript
+const fnCallback = (fn) => {
+   const info = ""
+   ...
+   fn(info);
+};
 ```
 
-## How to use
+###### Answer
 
-```bash
-git clone https://github.com/Nikodermus/frontend-code-challenge.git
-cd frontend-code-challenge
-npm install
-npm run start
+```javascript
+const fnCallback = async fn => {
+    const info = 'test';
+    Promise.resolve(fn(info));
+}
 ```
-
-The app will be available in your browser at `localhost:3000`
-
-## Challenges
-
-These are a series of features the app should do, here they are sorted in the priority they have, if you feel unable to complete one you can continue with the next one in hope to do as much as possible in given the time scope.
-
-1.  Fetch the given `.json` file inside `src/App.jsx` when the user writes in the input
-1.  Filter the results given user input, it can be searched by `Name` or one of its `Types`
-1.  Results should be not more than 4 at any time.
-1.  Sort elements first if the match the `Name`, then if they match the `Type`
-1.  User can toggle on/off to sort by maximum combat points (`MaxCP`), this will take effect on the whole list, but you should only show the first 4 elements that match the current search
-1.  If the search matches a part of the name, that part of the name should be hightlighted
-1.  While the list is being downloaded, show the loader.
-1.  If there're no results, show the `No Results` badge
-
-## Development
-
-- You can use internet, other projects, textbooks and whatever you think you need to complete the task, work as you usually would.
-- Please speak as you develop, this in order to understand better what you are trying to do.
-- You can ask questions if you need to, but try to refer to this document in first place, should have all what you need.
-- You can create as many components in the fasion you want (classes, stateless functional components, react hooks).
-- This setup uses EcmaScript Stage 4, you can use destructure, rest syntax, etc... This project is meant to be written with ES6+ standard.
-- Do not add dependencies.
-- Do not spend time in configuration, babel, webpack. This project has enough for its purpose and time is short.
--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,94 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 const URL_PATH = "https://gist.githubusercontent.com/bar0191/fae6084225b608f25e98b733864a102b/raw/dea83ea9cf4a8a6022bfc89a8ae8df5ab05b6dcc/pokemon.json";
 
-class App extends Component{ 
+class App extends Component {
 
-    state = {
-        pokemons: [],
-        filteredValues: [],
-        isLoading: true,
+  state = {
+    pokemons: [],
+    filteredValues: [],
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    fetch(URL_PATH)
+      .then(resp => resp.json())
+      .then(pokemons => this.setState({ pokemons, filteredValues: pokemons, isLoading: false }))
+      .catch(e => console.log(e))
+  }
+
+  searchPokemon = e => {
+    const { pokemons } = this.state;
+    let { value: searchItem } = e.target;
+    searchItem = searchItem.toLowerCase();
+    const regexp = new RegExp(searchItem, 'i');
+
+    if (!searchItem) {
+      this.setState({ filteredValues: pokemons });
+      return;
     }
 
-    componentDidMount() {
-        fetch(URL_PATH)
-            .then( resp => resp.json())
-            .then(pokemons => this.setState({pokemons, filteredValues: pokemons, isLoading: false}))
-            .catch(e => console.log(e))
-    }
+    const filteredValues = pokemons.filter(p => regexp.test(p.Name) || p.Types.some(t => regexp.test(t.toLowerCase())));
 
-    searchPokemon = e => {
-        const { pokemons } = this.state;
-        let { value: searchItem } = e.target;
-        searchItem = searchItem.toLowerCase();
-        const regexp = new RegExp(searchItem, 'i');
+    this.setState({
+      filteredValues: filteredValues.slice(0, 4).sort((a, b) => {
+        if (regexp.test(a.Name) || regexp.test(b.Name)) {
+          if (a.Name < b.Name) {
+            return -1;
+          }
 
-        if(!searchItem) {
-            this.setState({filteredValues: pokemons});
-            return;
+          return 1;
         }
-
-        const filteredValues  = pokemons.filter( p => regexp.test(p.Name) || p.Types.some( t => regexp.test(t.toLowerCase())));
-
-        this.setState({filteredValues: filteredValues.slice(0, 4).sort((a, b) => {
-            if(regexp.test(a.Name) || regexp.test(b.Name)) {
-                if(a.Name < b.Name) {
-                    return -1;
-                }
-    
-                return 1;
-            }
-            return 2;            
-        })})
-    }
+        return -2;
+      })
+    })
+  }
 
 
-    render(){
-        const {filteredValues, isLoading} = this.state;
-        const pokemonsElemnts = filteredValues.map( poke => (
-            <li key={poke.Number}>
-                <img src={poke.img} alt={poke.Name} />
-                <div className="info">
-                    <h1>
-                        <span className="hl">{poke.Name}</span>
-                    </h1>
-                    {
-                        poke.Types.map((t, i) => <span key={i} className={`type ${t.toLowerCase()}`}>{t}</span>)
-                    }
-                </div>
-            </li>
-        ));
+  render() {
+    const { filteredValues, isLoading } = this.state;
+    const pokemonsElemnts = filteredValues.map(poke => (
+      <li key={poke.Number}>
+        <img src={poke.img} alt={poke.Name} />
+        <div className="info">
+          <h1>
+            <span className="hl">{poke.Name}</span>
+          </h1>
+          {
+            poke.Types.map((t, i) => <span key={i} className={`type ${t.toLowerCase()}`}>{t}</span>)
+          }
+        </div>
+      </li>
+    ));
 
-        return (
-            <>
-            <label htmlFor="maxCP" className="max-cp">
-                <input type="checkbox" id="maxCP" />
-                <small>
-                    Maximum Combat Points
+    return (
+      <>
+        <label htmlFor="maxCP" className="max-cp">
+          <input type="checkbox" id="maxCP" />
+          <small>
+            Maximum Combat Points
                 </small>
-            </label>
-            <input type="text" className="input" placeholder="Pokemon or type" onKeyUp={this.searchPokemon} />
+        </label>
+        <input type="text" className="input" placeholder="Pokemon or type" onKeyUp={this.searchPokemon} />
 
-            {isLoading && <div className="loader"></div>}
-            
-            {!isLoading && <ul className="suggestions">
-                {pokemonsElemnts.length === 0 && <li>
-                    <img src="https://cyndiquil721.files.wordpress.com/2014/02/missingno.png" alt="" />
-                    <div className="info">
-                        <h1 className="no-results">
-                            No results
+        {isLoading && <div className="loader"></div>}
+
+        {!isLoading && <ul className="suggestions">
+          {pokemonsElemnts.length === 0 && <li>
+            <img src="https://cyndiquil721.files.wordpress.com/2014/02/missingno.png" alt="" />
+            <div className="info">
+              <h1 className="no-results">
+                No results
                         </h1>
-                    </div>
-                </li>}
-                {pokemonsElemnts}
-            </ul>}
-            
-            </>
-        )}
-    }
+            </div>
+          </li>}
+          {pokemonsElemnts}
+        </ul>}
+
+      </>
+    )
+  }
+}
 
 export default App;
